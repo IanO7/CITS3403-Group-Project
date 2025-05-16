05cf2b9d2a0327d921b9c5dade034de6aa380fa3
Iano7 <141983644+IanO7@users.noreply.github.com>
2025-05-16 11:56:42 +0800

    Organised demo data to folder but couldn't run file, just added sys.path so Python can run file.

--- demo/seed_demo_data.py
+++ demo/seed_demo_data.py
@@ -1,4 +1,7 @@
+import sys
 import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app import db, create_app
 from app.models import User, Note
 from werkzeug.security import generate_password_hash
